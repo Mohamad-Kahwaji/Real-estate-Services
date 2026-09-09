d77a8e758a7fe217ae9385e9145622863aceea43
Mohamad Kahwaji <mohamad.17.kahwaji@gmail.com>
2026-09-09 13:10:19 +0300

    \\

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \App\Http\Middleware\CheckPermission::class,
